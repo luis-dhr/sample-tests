5a7e1035ef6f8ba5561e43099ad44c0f058b639b
luis-dhr <luis-dhr@outlook.com>
2023-10-05 10:17:54 -0600

    fix: increase axios timeout

--- src/libraries/axios.js
+++ src/libraries/axios.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
 const axiosInstance = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com'
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  timeout: 20000
 })
 
 module.exports = axiosInstance
