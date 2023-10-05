4176d9c2d822d5c0904b05a62eaa953a988ea23d
luis-dhr <luis-dhr@outlook.com>
2023-10-04 18:22:02 -0600

    feat: add system tests

---
+++ tests/server/posts.test.js
@@ -0,0 +1,58 @@
+const request = require('supertest')
+const app = require('../../src/server')
+
+jest.useFakeTimers()
+let server
+
+beforeEach(() => {
+  server = app.listen(3000)
+})
+
+afterEach(() => {
+  server.close()
+})
+
+describe('API', () => {
+  describe('Posts', () => {
+    it('should get all posts', async () => {
+      const res = await request(server).get('/posts')
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('posts')
+    }, 10000)
+
+    it('should get a post by id', async () => {
+      const res = await request(server).get('/posts/1')
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('post')
+    }, 10000)
+
+    it('should create a new post', async () => {
+      const res = await request(server).post('/posts').send({
+        userId: 5,
+        title: 'New Post',
+        body: 'A new post created by the API'
+      })
+
+      expect(res.statusCode).toEqual(201)
+      expect(res.body).toHaveProperty('newPost')
+    }, 10000)
+
+    it('should update a post', async () => {
+      const res = await request(server).put('/posts/6').send({
+        title: 'Updated Post',
+        body: 'A post updated by the API'
+      })
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('postUpdated')
+    }, 10000)
+
+    it('should delete a post', async () => {
+      const res = await request(server).delete('/posts/9')
+
+      expect(res.statusCode).toEqual(204)
+    }, 10000)
+  })
+})
---
+++ tests/server/users.test.js
@@ -0,0 +1,59 @@
+const app = require('../../src/server')
+const request = require('supertest')
+
+jest.useFakeTimers()
+let server
+
+beforeEach(() => {
+  server = app.listen(3001)
+})
+
+afterEach(() => {
+  server.close()
+})
+
+describe('API', () => {
+  describe('Users', () => {
+    it('should get all users', async () => {
+      const res = await request(server).get('/users')
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('users')
+    }, 10000)
+
+    it('should get a user by id', async () => {
+      const res = await request(server).get('/users/1')
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('user')
+    }, 10000)
+
+    it('should create a new user', async () => {
+      const res = await request(server).post('/users').send({
+        name: 'John Doe',
+        username: 'john.doe',
+        email: 'mail@example.com'
+      })
+
+      expect(res.statusCode).toEqual(201)
+      expect(res.body).toHaveProperty('newUser')
+    }, 10000)
+
+    it('should update a user', async () => {
+      const res = await request(server).put('/users/1').send({
+        name: 'John Doe Jr.',
+        username: 'john.doe.jr',
+        email: 'john.doe@mail.com'
+      })
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('userUpdated')
+    }, 10000)
+
+    it('should delete a user', async () => {
+      const res = await request(server).delete('/users/5')
+
+      expect(res.statusCode).toEqual(204)
+    }, 10000)
+  })
+})
